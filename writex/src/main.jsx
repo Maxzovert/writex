dd6b457fe71d8e2a42ae08b10a383101a7e461e1
Abdullah <95abdullah99@gmail.com>
2025-06-12 18:30:32 +0530

    Refactor import statements in main.jsx for consistency and clarity by removing file extensions.

--- writex/src/main.jsx
+++ writex/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom';
-import { Pointer } from "./components/magicui/pointer.js"
+import { Pointer } from "./components/magicui/pointer"
 import { ToastContainer, toast } from 'react-toastify';
-import { AuthProvider } from "./context/AuthContext.jsx"
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
