aa7b1c1bd3204c0b65f0e82e5956a1f306756077
Abdullah <95abdullah99@gmail.com>
2025-06-26 21:48:16 +0530

    Enhance blog writing features and UI components
    - Added new dependencies including @floating-ui/react and sass-embedded for improved UI functionality.
    - Implemented new Tiptap extensions for link, selection, and trailing nodes to enhance text editing capabilities.
    - Created various icon components for text formatting options (alignments, bold, italic, etc.) to improve the editor's toolbar.
    - Developed new components for handling image uploads and code blocks, enhancing the blog writing experience.
    - Updated styles and added keyframe animations for smoother UI transitions.
    - Introduced a theme toggle feature for better user experience in dark and light modes.

--- writex/src/App/WriteBlog/WriteBLog.jsx
+++ writex/src/App/WriteBlog/WriteBLog.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from "../Components/Navbar"
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
+import { Button } from "../../components/ui/button"
 
-const INITIAL_DATA = {
-  blocks :[
-
-  ]
-}
 const WriteBLog = () => {
-  // const [data , setData] = useState(INITIAL_DATA);
   return (
-    <div>
-        <Navbar/>
-      Write Blog
-        {/* <Editor data={data} onChange={setData} editorBlock="editorjs"/> */}
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      <div className="flex flex-row flex-1 min-h-0">
+        <div className="w-1/2 bg-gray-300 mt-20 rounded-r-4xl">
+
+        </div>
+        {/* Editor */}
+        <div className="w-1/2 flex flex-col">
+          <h1 className="text-center font-bold text-3xl mt-5 mb-2">Start Writing</h1>
+          <div className="flex-1 min-h-0 overflow-y-auto flex flex-col">
+            <SimpleEditor />
+            <div className="flex justify-center mt-4 mb-4">
+              <Button className="text-center">Publish Now</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
