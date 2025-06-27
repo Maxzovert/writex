55232f5ff3e0db653945fc1146157f86f39d29dc
Abdullah <95abdullah99@gmail.com>
2025-06-27 19:22:38 +0530

    Fix API endpoint in authContext.jsx to ensure correct URL format for user authentication requests.

--- writex/src/context/authContext.jsx
+++ writex/src/context/authContext.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({children}) => {
         const checkAuth = async () => {
             try {
                 // const response = await axios.get('api/users/current');
-                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}api/users/current`);
+                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/current`);
                 setUser(response.data)
             } catch (error) {
                 setUser(null);
