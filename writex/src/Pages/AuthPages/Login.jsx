bf836345fbeac3b8d5e88be6c956d66b05fc439b
Abdullah <95abdullah99@gmail.com>
2025-07-11 16:37:06 +0530

    Update CORS settings for production and refine API endpoint usage
    - Updated CORS configuration in index.js to enable production settings for the backend.
    - Refined API endpoint usage in Login.jsx and authContext.jsx to consistently utilize environment variables.
    - Commented out previous local endpoint references for clarity and future reference.

--- writex/src/Pages/AuthPages/Login.jsx
+++ writex/src/Pages/AuthPages/Login.jsx
@@ -30,8 +30,8 @@ const Login = () => {
     }
 
     try {
-      // const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/login`, {
-      const response = await axios.post('/api/users/login', {
+      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/login`, {
+      // const response = await axios.post('/api/users/login', {s
         email: formData.email,
         password: formData.password
       }, {
